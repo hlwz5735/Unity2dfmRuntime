6417c5793f19cffc1604ce5189b82702db682a36
limeng <limengwwwq@163.com>
2023-08-22 16:43:48 +0800

    feat: 声音播放

---
+++ Assets/Scripts/WavFilePlayer.cs
@@ -0,0 +1,44 @@
+using System;
+using _2dfmFile;
+using Attributes;
+using Audio;
+using Data;
+using Game;
+using UnityEngine;
+
+public class WavFilePlayer : MonoBehaviour
+{
+    private PlayerData playerData;
+
+    public AudioSource audioSource;
+
+
+    [CustomLabel("声音序号")] public int soundNo = 0;
+
+    private void Start()
+    {
+        this.audioSource = GetComponent<AudioSource>();
+        var playerFilePath = Application.streamingAssetsPath + "/Players/DONGDONG.player";
+        this.playerData = PlayerFileReader.read2dfmPlayerFile(playerFilePath);
+    }
+
+    private AudioClip getAudioClip()
+    {
+        int idx = Math.Min(this.playerData.soundCount - 1, Math.Max(0, this.soundNo));
+        var wavBytes = playerData.sounds[idx].bytes;
+        var wav = new WaveFile(wavBytes);
+        AudioClip audioClip = AudioClip.Create("testSound", wav.SampleCount, 1, wav.Frequency, false);
+        audioClip.SetData(wav.LeftChannel, 0);
+        return audioClip;
+    }
+
+    public void playAudio()
+    {
+        var clip = this.getAudioClip();
+        if (clip != null)
+        {
+            audioSource.clip = clip;
+            this.audioSource.Play();
+        }
+    }
+}
